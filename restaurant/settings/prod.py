dd49d2b466738548cda046848c344a8d61094b07
jaewook125 <jaewook125@naver.com>
2017-05-08 17:59:11 +0900

    prod 수정

--- restaurant/settings/prod.py
+++ restaurant/settings/prod.py
@@ -1,2 +1,13 @@
 from .common import *
 from .conf import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        'PASSWORD': DB_PW,
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
