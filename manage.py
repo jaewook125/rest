65b28b45b3eda56ff0c8cdaf1f3d24b664d9cbc3
jaewook125 <jaewook125@naver.com>
2017-05-09 15:27:33 +0900

    manage.py

--- manage.py
+++ manage.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 import os
 import sys
+from restaurant.settings.conf import DEVPROD
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "restaurant.settings.dev")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", DEVPROD)
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
